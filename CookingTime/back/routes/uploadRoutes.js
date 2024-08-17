9e27ab0c6517f54cfec1d6afb998151eb5b129e2
sergionago <sergionago96@gmail.com>
2024-08-17 22:01:06 +0200

    Delete CookingTime directory
    deleted cooking time

--- CookingTime/back/routes/uploadRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const uploadController = require('../controllers/uploadController')
-
-router.post('/images', uploadController.uploadImages)
-
-module.exports = router;
